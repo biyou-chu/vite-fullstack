208a276e81839025a62979098a5e612b10edb4b9
mac <mac@macs-Mac-mini.local>
2025-10-23 18:12:55 +0800

    前端构建Vite通用构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
-import injectChuPatch from './vite-plugin-inject-chu-patch'; // 引入 patch 插件
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const projectId = env.VITE_PROJECT_ID || '';
+  const projectId = env.VITE_CHU_PROJECT_ID || '';
 
   return {
-    base: `/${projectId}`,
+    // 核心：设置 base 为 /projectId/，Vite 自动给 CSS 资源加前缀
+    base: projectId ? `/${projectId}/` : '/', 
     plugins: [
       react(),
-      injectChuPatch(), // 注入插件
     ],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': path.resolve(__dirname, './app'),
       },
     },
     optimizeDeps: {
